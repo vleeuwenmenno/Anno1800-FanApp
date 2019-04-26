0083badf67aa421b0037643d7dc34078c5dd5737
Dunccan Groenendijk <dunccan@revenyou.io>
2019-04-26 15:23:56 +0200

    added rss feed plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:anno1800_fanapp/pages/splash.dart';
+import 'package:anno1800_fanapp/pages/newsFeed.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,17 +8,65 @@ class MyApp extends StatelessWidget
 {
 	// This widget is the root of your application.
 	@override
-	Widget build(BuildContext context) 
+	Widget build(BuildContext context)
 	{
+		MaterialColor darkBrown = const MaterialColor(
+			//#714F28
+			0xff714F28, const
+			{
+				50 : const Color(0xff714F28),
+				100 : const Color(0xff714F28),
+				200 : const Color(0xff714F28),
+				300 : const Color(0xff714F28),
+				400 : const Color(0xff714F28),
+				500 : const Color(0xff714F28),
+				600 : const Color(0xff714F28),
+				700 : const Color(0xff714F28),
+				800 : const Color(0xff714F28),
+				900 : const Color(0xff714F28)
+			}
+		);
+
+		MaterialColor sand = const MaterialColor(
+			//#FFE4AD
+			0xffFFE4AD, const 
+			{
+				50 : const Color(0xffFFE4AD),
+				100 : const Color(0xffFFE4AD),
+				200 : const Color(0xffFFE4AD),
+				300 : const Color(0xffFFE4AD),
+				400 : const Color(0xffFFE4AD),
+				500 : const Color(0xffFFE4AD),
+				600 : const Color(0xffFFE4AD),
+				700 : const Color(0xffFFE4AD),
+				800 : const Color(0xffFFE4AD),
+				900 : const Color(0xffFFE4AD)
+			}
+		);
+
 		return MaterialApp(
 			title: 'Flutter Demo',
 			theme: ThemeData(
-				primarySwatch: Colors.blue,
+				primarySwatch: darkBrown,
+				canvasColor: sand,
+				appBarTheme: AppBarTheme(
+					textTheme: TextTheme(
+						title: TextStyle(
+							color: sand,
+							fontSize: 20,
+							fontWeight: FontWeight.w500
+						)
+					),
+					iconTheme: IconThemeData(color: sand),
+
+				)
 			),
-			initialRoute: '/',
+			initialRoute: '/newsFeed',
 			routes: 
 			{
 				'/': (context) => SplashScreen(),
+
+				'/newsFeed': (context) => NewsFeed(),
 			},
 		);
 	}
---
+++ lib/pages/newsFeed.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:webfeed/webfeed.dart';
+import 'package:http/http.dart' as http;
+
+class NewsFeed extends StatefulWidget 
+{
+  
+  @override
+  _NewsFeedState createState() => _NewsFeedState();
+
+}
+
+class _NewsFeedState extends State<NewsFeed> 
+{
+	Widget build(BuildContext context)
+	{
+		var client = new http.Client();
+		ScreenUtil.instance = ScreenUtil.getInstance()..init(context);
+		client.get("https://www.anno-union.com/en/category/devblog-en/feed").then((response) {
+    	return response.body;
+		}).then((bodyString) {
+			var channel = new RssFeed.parse(bodyString);
+			print(channel);
+			return channel;
+		});
+		return Scaffold(
+			appBar: AppBar(
+				title: Text('Anno 1800 - News'),
+				actions: <Widget>
+				[
+					IconButton(
+						onPressed: () 
+						{
+							print('object');
+						},
+						icon: Icon(
+							Icons.search, color: Color(0xffFFE4AD)
+						)
+					)
+				],
+			),
+			// body: ListView.builder(),
+			// body: Container(
+			// 	child: Text('data'),
+			// ),
+		);
+	}
+}
