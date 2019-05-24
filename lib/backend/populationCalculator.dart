895c5af589b3d33e74e3443c92bf15d2b618d84c
Dunccan <dunccan@revenyou.io>
2019-05-24 13:20:09 +0200

    Added needs for engineers and investors

--- lib/backend/populationCalculator.dart
+++ lib/backend/populationCalculator.dart
@@ -8,7 +8,7 @@ class PopulationCalculator
 			"Marketplace":  0.0,
 			"Residence":    0.1,
 			"Fish":         0.0025,
-			"Work_clothes": 0.00307692,
+			"Work_clothes":	0.00307692,
 			"luxury": 
 			{
 				"Schnapps":  0.00333334,
@@ -21,7 +21,7 @@ class PopulationCalculator
 			"Marketplace":  0.0,
 			"Residence":    0.05,
 			"Fish":         0.0025,
-			"Work_clothes": 0.00307692,
+			"Work_clothes":	0.00307692,
 			"Schnapps":     0.00333334,
 			"Sausages":     0.001000002,
 			"Bread":        0.00090909,
@@ -37,30 +37,67 @@ class PopulationCalculator
 
 		"artisan":
 		{
-			"Residence":       0.0333333333,
-			"Sausages":        0.001333334,
-			"Bread":           0.001212122,
-			"Soap":            0.000555556,
-			"Canned_food":     0.00034188,
-			"Sewing_machines": 0.00095238,
-			"Fur_Coats":       0.000888888,
+			"Residence":       	0.0333333333,
+			"Sausages":        	0.001333334,
+			"Bread":           	0.001212122,
+			"Soap":           	0.000555556,
+			"Canned_food":    	0.00034188,
+			"Sewing_machines":	0.00095238,
+			"Fur_Coats":      	0.000888888,
 			"luxury": 
 			{
-				"Church":          0.0,
-				"Beer":            0.001025642,
-				"Variety_theatre": 0.0,
-				"Rum":             0.001904762,
+				"Church":         	0.0,
+				"Beer":				0.001025642,
+				"Variety_theatre":	0.0,
+				"Rum":				0.001904762,
 			}
 		},
 
 		"engineer":
 		{
-
+			"Residence":		0.025,
+			"Canned_food":		0.00051282,
+			"Sewing_machines":	0.0014285715,
+			"Fur_coats":		0.0013333335,
+			"University":		0.0,
+			"Glasses":			0.000222222,
+			"Coffee":			0.0011764710000000001,
+			"Pocket_watches":	0.0001960785,
+			"Electricity":		0.0,
+			"Light_bulbs":		0.0003124995,
+			"luxury": 
+			{
+				"Variety_theatre":		0.0,
+				"Rum":					0.002857143,
+				"High_wheelers":		0.0006250005000000001,
+				"Pocket_watches":		0.0001960785,
+				"Bank":					0.0,
+			}
 		},
 
 		"investor":
 		{
-
+			"Residence":		0.02,
+			"Glasses":			0.0003555552,
+			"High_wheelers":	0.0009999995999999999,
+			"Coffee":			0.0018823524,
+			"Pocket_watches":	0.0003137256,
+			"Electricity":		0.0,
+			"Light_bulbs":		0.0005000004,
+			"Bank":				0.0,
+			"Champagne":		0.0004704,
+			"Cigars":			0.000444444,
+			"Chocolate":		0.0010666668,
+			"Steam_carriages":	0.00013333319999999998,
+			"luxury": 
+			{
+				"High_wheelers":	0.0006250005000000001,
+				"Pocket_watches":	0.0001960785,
+				"Bank":				0.0,
+				"Jewelry":			0.00042105239999999997,
+				"Gramophones":		0.00010524,
+				"Members_club":		0.0,
+			}
 		}
 	};
 
